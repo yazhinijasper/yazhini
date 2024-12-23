26a2d49e2c19ad34a9760451bdaa93721b0cc205
yazhinijasper <yazhinijasper@gmail.com>
2024-12-23 20:29:22 +1300

    Add files via upload

---
+++ yazhiniportfolio.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer",],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("html-skill").style.width = "90%";
+    document.getElementById("css-skill").style.width = "80%";
+    document.getElementById("js-skill").style.width = "70%";
+});
+
